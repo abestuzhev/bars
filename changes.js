975c72245a12b0a78f3fb74c8d0dff6b6cd0d573
Aleksey <aleksey.bestuzhev@gmail.com>
2017-03-11 09:54:04 +0300

    changes

---
+++ changes.js
@@ -0,0 +1,7 @@
+// $( document ).ready(function() {
+//    $(".spares-card").hover(function(){
+//       $(".spares-card_title").removeClass("hover-card");
+//       $(this).find(".spares-card_title").addClass("hover-card");
+//    });
+// });
+
